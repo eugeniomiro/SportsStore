d6b908f1376399b8eb0958f42d528ccd1af89f29
Eugenio Miró <eugenio.miro@gmail.com>
2021-05-04 09:32:40 -0300

    mover comentarios a README.md eliminar bundle de modernizr que no se usa agregar inicializacion de npm agregar README.md a la solución

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ This is a collection of links found in the generated code which I wanted to remo
         <httpRuntime targetFramework="4.5.1" />
       </system.Web>
 ```
+
+### SportsStore.WebUI/App_Start/BundleConfig.cs
+
+* For more information on bundling, visit [this](http://go.microsoft.com/fwlink/?LinkId=301862)
+* Use the development version of Modernizr to develop with and learn from. Then, when you're
+  ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- SportsStore.WebUI/App_Start/BundleConfig.cs
+++ SportsStore.WebUI/App_Start/BundleConfig.cs
@@ -4,7 +4,6 @@ namespace SportsStore.WebUI
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -12,11 +11,6 @@ namespace SportsStore.WebUI
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
