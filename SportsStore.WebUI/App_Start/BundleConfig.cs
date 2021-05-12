869dd3a8dce260e1ead1c98225d0dc023dd09389
Eugenio Miró <eugenio.miro@gmail.com>
2021-05-12 16:34:12 -0300

    remove unused javascript code dependencies

--- SportsStore.WebUI/App_Start/BundleConfig.cs
+++ SportsStore.WebUI/App_Start/BundleConfig.cs
@@ -6,12 +6,6 @@ namespace SportsStore.WebUI
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
 
