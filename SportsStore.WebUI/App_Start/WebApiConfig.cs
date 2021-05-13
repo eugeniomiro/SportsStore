f54b37cf5485ddf42eac0b717176c4f13c457cfa
Eugenio Miró <eugenio.miro@gmail.com>
2021-05-12 22:06:08 -0300

    Enable CORS for ASP.NET and Angular

---
+++ SportsStore.WebUI/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SportsStore.WebUI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
+        }
+    }
+}
